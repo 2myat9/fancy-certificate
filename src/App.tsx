1e72100b88939da5ad11e17912edb755ad6e744c
Htoomyat Naing <2myat9@gmail.com>
2021-06-30 21:51:42 -0700

    Deploy first working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css";
+import Tilt from "react-parallax-tilt";
+import Confetti from "react-confetti";
 
-function App() {
+const Card = () => {
+  const [isConfettiVisible, setisConfettiVisible] = React.useState(false);
+
+  const toggleConfetti = (): void => {
+    setisConfettiVisible((prevState) => !prevState);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container">
+        <Tilt
+          className="tilt-root"
+          glareEnable="true"
+          glareMaxOpacity="1"
+          scale="1.1"
+          tiltReverse="true"
+          perspective="800"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {/* I made card content separate so that I can enable confetti only on contents */}
+          <CardContent
+            isConfettiVisible={isConfettiVisible}
+            toggleConfetti={toggleConfetti}
+          />
+        </Tilt>
+      </div>
+    </>
   );
+};
+
+interface CardContentProps {
+  isConfettiVisible: boolean;
+  toggleConfetti: () => void;
 }
 
-export default App;
+const CardContent: React.FC<CardContentProps> = (props) => {
+  return (
+    <>
+      {/* confetti is manually given size equal to tilt-child div */}
+      {props.isConfettiVisible ? <Confetti width={650} height={700} /> : null}
+      <div
+        className="tilt-child"
+        onMouseEnter={props.toggleConfetti}
+        onMouseLeave={props.toggleConfetti}
+      >
+        <div className="totally-centered">
+          <div className="content-container">
+            <img
+              className="cover-img"
+              src="https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              alt="cover-img"
+            />
+            <p className="header">Header!</p>
+            <p className="content">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Necessitatibus nostrum, ipsam dolores assumenda aspernatur quam
+              omnis sapiente cupiditate optio eveniet, iure officiis. Modi
+              molestiae, corporis aperiam placeat laborum vero nobis!
+            </p>
+            <p>Footer</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
---
+++ src/types/alltypes.d.ts
@@ -0,0 +1,4 @@
+declare module "react-parallax-tilt";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
